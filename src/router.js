5c3cdbffaab5c3072ff7f3aa30d3fe0c284429c0
AbsharNP <absharnc@gmail.com>
2024-04-25 21:18:08 +0530

    second commit

--- src/router.js
+++ src/router.js
@@ -1,17 +1,1 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HomeMsg from '@/components/Home.vue'
-import LoginPage from '@/components/Login.vue'
-Vue.use(Router)
-
-export default new Router({
-    routes: [
-        {
-            path: '/', components:HomeMsg
-        },
-        {
-            path: '/login', components:LoginPage
-        }
-        
-    ]
-})
+ 
